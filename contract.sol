ba6b0f6e3b25a931fb82fe930e97cd3cc8785ed0
Dmitry Velichko <soleil-2005@mail.ru>
2022-06-02 18:48:43 +0300

    new commit

--- contract.sol
+++ contract.sol
@@ -1,5 +1,8 @@
 
 export const deploy = async (contractName: string, arguments: Array<any>, from?: string) => {    
     
+    console.log(`deploying ${contractName}`)
+    // Note that the script needs the ABI which is generated from the compilation artifact.
+    // Make sure contract is compiled and artifacts are generated
    
 }: Promise<any>
