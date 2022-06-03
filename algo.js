d2d481dd1ffb79db06e6f6014a171205a89de746
Dmitry Velichko <soleil-2005@mail.ru>
2022-06-03 23:15:45 +0300

    new commit

--- algo.js
+++ algo.js
@@ -16,7 +16,10 @@ function oddArray(arr) {
         } else if(arr[0] % 2 !== 0) {
             result.push(arr[0]); // 2
         }
-        
+        helperRecursiveFn(arr.slice(1)); // 3
+    }
+    helperRecursiveFn(arr);
+    return result;
 }
 
 oddArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
