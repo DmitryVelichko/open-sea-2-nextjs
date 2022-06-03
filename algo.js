c7c7904958b8c37d1178d2d32625989beb12c3ac
Dmitry Velichko <soleil-2005@mail.ru>
2022-06-03 23:17:16 +0300

    new commit

--- algo.js
+++ algo.js
@@ -1,19 +1,5 @@
-function calculateLpsTable(subStr) {
-    let i = 1;
-    let j = 0;
-    let lps = new Array(subStr.length).fill(0);
+function mergeSortedAlgo(array) {
+    if(array.length <= 1) return array;
 
-    while(i < subStr.length) {
-        if(subStr[i] === subStr[j]) {
-            lps[i] = j + 1;
-            i += 1;
-            j += 1;
-        } else {
-            if(j !== 0) {
-                j = lps[j - 1];
-            } else {
-                i += 1;
-            }
-        }
-    }
-    return lps;
+    let midPoint = Math.floor(array.length / 2);
+    
