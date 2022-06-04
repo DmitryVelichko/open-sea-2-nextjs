59ddfccc8d83f0527863509d89b6c60beee68118
Dmitry Velichko <soleil-2005@mail.ru>
2022-06-04 23:27:17 +0300

    new commit

--- algo.js
+++ algo.js
@@ -1,9 +1,4 @@
-function mergeSortedAlgo(array) {
-    if(array.length <= 1) return array;
+function recursiveFn() {
+    
 
-    let midPoint = Math.floor(array.length / 2);
-    let leftArray = mergeSortedAlgo(array.slice(0, midPoint));
-    let rightArray = mergeSortedAlgo(array.slice(midPoint));
-
-    return mergeSortedArray(leftArray, rightArray);
-}
+recursiveFn();
