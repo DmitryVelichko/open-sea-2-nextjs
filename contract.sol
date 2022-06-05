1bdb12cf0cf9cda52751e278ca5f0a4ef7224a51
Dmitry Velichko <soleil-2005@mail.ru>
2022-06-05 22:14:31 +0300

    new commit

--- contract.sol
+++ contract.sol
@@ -11,6 +11,16 @@ contract SimpleStorage {
 
     mapping(string => uint256) public nameToFavoriteNumber;
 
- 
+    function store(uint256 _favoriteNumber) public virtual {
+        favoriteNumber = _favoriteNumber;
+    }
+    
+    function retrieve() public view returns (uint256){
+        return favoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        people.push(People(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
     }
 }
