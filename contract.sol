1b08533945163b5d8555ba08d3697eac3bfd7c99
Dmitry Velichko <soleil-2005@mail.ru>
2022-06-06 21:41:53 +0300

    new commit

--- contract.sol
+++ contract.sol
@@ -1,4 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
 contract ReceiverPays {
-   
+    address owner = msg.sender;
+
+    mapping(uint256 => bool) usedNonces;
+
+    constructor() payable {}
