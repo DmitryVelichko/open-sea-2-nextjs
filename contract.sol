b6ef1eae1278f5e8269e8f62ff3033deae6e9113
Dmitry Velichko <soleil-2005@mail.ru>
2022-06-06 21:43:13 +0300

    new commit

--- contract.sol
+++ contract.sol
@@ -36,3 +36,8 @@ contract ReceiverPays {
         assembly {
             // first 32 bytes, after the length prefix.
             r := mload(add(sig, 32))
+            // second 32 bytes.
+            s := mload(add(sig, 64))
+            // final byte (first byte of the next 32 bytes).
+            v := byte(0, mload(add(sig, 96)))
+        }
