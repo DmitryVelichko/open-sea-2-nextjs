d4aad918931415c46022eda4f769dd1805e39596
Dmitry Velichko <soleil-2005@mail.ru>
2022-06-07 23:55:49 +0300

    new commit

--- algo.js
+++ algo.js
@@ -13,3 +13,14 @@ export default class HashTable {
   constructor(hashTableSize = defaultHashTableSize) {
     // Create hash table of certain size and fill each bucket with empty linked list.
     this.buckets = Array(hashTableSize).fill(null).map(() => new LinkedList());
+
+    // Just to keep track of all actual keys in a fast way.
+    this.keys = {};
+  }
+
+  /**
+   * Converts key string to hash number.
+   *
+   * @param {string} key
+   * @return {number}
+   */
