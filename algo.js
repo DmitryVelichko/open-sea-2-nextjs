44594e6d98533400bf233379fa94abb620873fa9
Dmitry Velichko <soleil-2005@mail.ru>
2022-06-07 23:57:35 +0300

    new commit

--- algo.js
+++ algo.js
@@ -25,3 +25,20 @@ export default class Heap {
   getLeftChildIndex(parentIndex) {
     return (2 * parentIndex) + 1;
   }
+
+  /**
+   * @param {number} parentIndex
+   * @return {number}
+   */
+  getRightChildIndex(parentIndex) {
+    return (2 * parentIndex) + 2;
+  }
+
+  /**
+   * @param {number} childIndex
+   * @return {number}
+   */
+  getParentIndex(childIndex) {
+    return Math.floor((childIndex - 1) / 2);
+  }
+
