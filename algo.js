ec9feac20e6183c7affceb1774b0893d781ba85f
Dmitry Velichko <soleil-2005@mail.ru>
2022-06-10 22:19:11 +0300

    new commit

--- algo.js
+++ algo.js
@@ -17,4 +17,7 @@ const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 function binarySearch(array, item) {
   let start = 0;
   let end = array.length;
+  let middle;
+  let found = false;
  
+console.log(binarySearch(array, 2));
