6453c9a0d020612d269b76228d45e2f35db599fe
Dmitry Velichko <soleil-2005@mail.ru>
2022-06-10 22:17:15 +0300

    new commit

--- algo.js
+++ algo.js
@@ -1,4 +1,10 @@
 const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 function linearSearch(array, item) {
+  for (let i = 0; i < array.length; i++) {
+    
+    if (array[i] === item) {
+      return i;
+    }
+  }
   
