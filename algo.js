be544153a933b56b42d9242f8f1c556aa71d4b3a
Dmitry Velichko <soleil-2005@mail.ru>
2022-06-11 22:09:55 +0300

    new commit

--- algo.js
+++ algo.js
@@ -22,4 +22,10 @@ export default class PriorityQueue extends MinHeap {
    * @param {number} [priority] - items priority.
    * @return {PriorityQueue}
    */
+  add(item, priority = 0) {
+    this.priorities.set(item, priority);
+    super.add(item);
+    return this;
+  }
+
  
