b1865eea0e6d7fb5b1c39ac7c023f69db53a6bad
Dmitry Velichko <soleil-2005@mail.ru>
2022-06-11 22:09:32 +0300

    new commit

--- algo.js
+++ algo.js
@@ -1,3 +1,9 @@
 import MinHeap from '../heap/MinHeap';
 import Comparator from '../../utils/comparator/Comparator';
 
+// It is the same as min heap except that when comparing two elements
+// we take into account its priority instead of the element's value.
+export default class PriorityQueue extends MinHeap {
+  constructor() {
+    // Call MinHip constructor first.
+    super();
