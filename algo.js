def2db1f6b5a686d4336ba1c17f7b3bf34ede06b
Dmitry Velichko <soleil-2005@mail.ru>
2022-06-12 23:34:32 +0300

    new commit

--- algo.js
+++ algo.js
@@ -6,3 +6,9 @@ import PriorityQueue from '../../../data-structures/priority-queue/PriorityQueue
  * @property {Object} previousVertices - shortest paths to all vertices.
  */
 
+/**
+ * Implementation of Dijkstra algorithm of finding the shortest paths to graph nodes.
+ * @param {Graph} graph - graph we're going to traverse.
+ * @param {GraphVertex} startVertex - traversal start vertex.
+ * @return {ShortestPaths}
+ */
