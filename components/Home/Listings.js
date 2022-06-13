0062347780ea6733f2ea281e11f8c0090ed6b245
Dmitry Velichko <soleil-2005@mail.ru>
2022-06-13 23:53:20 +0300

    new commit

--- components/Home/Listings.js
+++ components/Home/Listings.js
@@ -1,44 +1,52 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-import Link from 'next/link'
-import { useMarketplace } from '@thirdweb-dev/react'
-import NFTCard from './NFTCard'
+import React from 'react';
+import { useState, useEffect } from 'react';
+import Link from 'next/link';
+import { useMarketplace } from '@thirdweb-dev/react';
+import NFTCard from './NFTCard';
 
 const style = {
-
-}
+    wrapper: `mx-auto`
+};
 
 const Listings = () => {
-    const [listings, setListings] = useState([]);
-    const marketplace = useMarketplace('0x43055a8Cd7613227f4450B66F91287D055B0DaF9');
-    
+  const [listings, setListings] = useState([]);
+  const marketplace = useMarketplace(
+    '0x43055a8Cd7613227f4450B66F91287D055B0DaF9'
+  );
 
-    useEffect(() => {
-        getListings()
-    }, [])
+  useEffect(() => {
+    getListings();
+  }, []);
 
-    const getListings = async () => {
-        try {
-            const list = await marketplace.getActiveListings()
-            setListings(list)
-        } catch(error) {
-            console.log("Error!!! " + error)
-        }
+  const getListings = async () => {
+    try {
+      const list = await marketplace.getActiveListings();
+      setListings(list);
+    } catch (error) {
+      console.log('Error!!! ' + error);
     }
+  };
 
   return (
     <div className={style.wrapper}>
-        {listings.length > 0 ? (
-            <>
-               {listings?.map((listing) => (
+      {listings.length > 0 ? (
+        <>
+          {listings?.map((listing, index) => (
+            <Link
+              href={`/assets/${listing.assetContractAddress}/${listing.id}`}
+              key={index}
+            >
+              <a>
                 <NFTCard />
-               ))}
-            </>
-        ) : (
-            <div>Loading...</div>
-        )}
+              </a>
+            </Link>
+          ))}
+        </>
+      ) : (
+        <div>Loading...</div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Listings
+export default Listings;
