69f4707dc264588db6eb4366ce0ad3e92845fe6d
Dmitry Velichko <soleil-2005@mail.ru>
2022-06-13 10:28:04 +0300

    new commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,18 @@
-
-import {ChainId, ThirdwebProvider} from '@thirdweb-dev/react'
-import '../styles/globals.css'
+import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThirdwebProvider
       desiredChainId={ChainId.Rinkeby}
+      chainRpc={{
+        [ChainId.Rinkeby]:
+          'https://rinkeby.infura.io/v3/83b1c88d6e484420b12398819ca639d0',
+      }}
     >
       <Component {...pageProps} />
-      </ThirdwebProvider>
-  )
+    </ThirdwebProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
