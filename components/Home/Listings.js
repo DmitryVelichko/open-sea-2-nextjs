4be165281b20a99a50d15f957739912558710e8f
Dmitry Velichko <soleil-2005@mail.ru>
2022-06-13 17:08:49 +0300

    new commit

--- components/Home/Listings.js
+++ components/Home/Listings.js
@@ -4,6 +4,20 @@ import Link from 'next/link'
 import { useMarketplace } from '@thirdweb-dev/react'
 
 const Listings = () => {
+    const marketplace = useMarketplace('0x43055a8Cd7613227f4450B66F91287D055B0DaF9');
+
+    useEffect(() => {
+        getListings()
+    }, [])
+
+    const getListings = async () => {
+        try {
+            const list = await marketplace.getActiveListings()
+        } catch(error) {
+            console.log("Error!!! " + error)
+        }
+    }
+
   return (
     <div>Listings</div>
   )
