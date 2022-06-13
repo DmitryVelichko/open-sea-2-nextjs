12d4201bc8337c7fd8811625636fb45296c930c4
Dmitry Velichko <soleil-2005@mail.ru>
2022-06-13 17:01:15 +0300

    new commit

---
+++ components/Home/Listings.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import Link from 'next/link'
+import { useMarketplace } from '@thirdweb-dev/react'
+
+const Listings = () => {
+  return (
+    <div>Listings</div>
+  )
+}
+
+export default Listings
