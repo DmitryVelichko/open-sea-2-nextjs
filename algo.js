fa8fd0b0cd6747148127289407a7f50a40336de4
Dmitry Velichko <soleil-2005@mail.ru>
2022-06-15 23:50:59 +0300

    new commit

--- algo.js
+++ algo.js
@@ -2,3 +2,8 @@ import TrieNode from './TrieNode';
 
 // Character that we will use for trie tree root.
 const HEAD_CHARACTER = '*';
+
+export default class Trie {
+  constructor() {
+    this.head = new TrieNode(HEAD_CHARACTER);
+  }
