10640402ab9bfc44a680b5583696d14dc372fd92
Dmitry Velichko <soleil-2005@mail.ru>
2022-06-17 23:32:14 +0300

    new commit

--- algo.js
+++ algo.js
@@ -9,3 +9,9 @@
 
   // Init permutations array.
   const permutations = [];
+
+  // Get all permutations for permutationOptions excluding the first element.
+  const smallerPermutations = permutateWithoutRepetitions(permutationOptions.slice(1));
+
+  // Insert first option into every possible position of every smaller permutation.
+  const firstOption = permutationOptions[0];
