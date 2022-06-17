62f92fe2948ce5ecdeea4e58e30537a1380c4132
Dmitry Velichko <soleil-2005@mail.ru>
2022-06-17 23:32:29 +0300

    new commit

--- algo.js
+++ algo.js
@@ -15,3 +15,17 @@
 
   // Insert first option into every possible position of every smaller permutation.
   const firstOption = permutationOptions[0];
+
+  for (let permIndex = 0; permIndex < smallerPermutations.length; permIndex += 1) {
+    const smallerPermutation = smallerPermutations[permIndex];
+
+    // Insert first option into every possible position of smallerPermutation.
+    for (let positionIndex = 0; positionIndex <= smallerPermutation.length; positionIndex += 1) {
+      const permutationPrefix = smallerPermutation.slice(0, positionIndex);
+      const permutationSuffix = smallerPermutation.slice(positionIndex);
+      permutations.push(permutationPrefix.concat([firstOption], permutationSuffix));
+    }
+  }
+
+  return permutations;
+}
