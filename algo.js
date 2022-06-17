a33b7efa7c85c68dfb597be55c3a289a2ddbefe6
Dmitry Velichko <soleil-2005@mail.ru>
2022-06-17 23:30:32 +0300

    new commit

--- algo.js
+++ algo.js
@@ -5,3 +5,8 @@
  */
  export default function permutateWithRepetitions(
   permutationOptions,
+  permutationLength = permutationOptions.length,
+) {
+  if (permutationLength === 1) {
+    return permutationOptions.map((permutationOption) => [permutationOption]);
+  }
