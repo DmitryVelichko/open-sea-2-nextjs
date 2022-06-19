dc197ca4a12ae605131def4be4c5db31bdab90ef
Dmitry Velichko <soleil-2005@mail.ru>
2022-06-19 22:55:35 +0300

    new commit

---
+++ algo.js
@@ -0,0 +1,11 @@
+import BinarySearchTree from '../binary-search-tree/BinarySearchTree';
+
+// Possible colors of red-black tree nodes.
+const RED_BLACK_TREE_COLORS = {
+  red: 'red',
+  black: 'black',
+};
+
+// Color property name in meta information of the nodes.
+const COLOR_PROP_NAME = 'color';
+
