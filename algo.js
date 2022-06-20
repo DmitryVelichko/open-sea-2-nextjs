c05b1e5f6b3a5a7a32fb1b85eb100fd8c6eeb2d4
Dmitry Velichko <soleil-2005@mail.ru>
2022-06-20 23:13:56 +0300

    new commit

--- algo.js
+++ algo.js
@@ -4,3 +4,7 @@ export default class DisjointSet {
   /**
    * @param {function(value: *)} [keyCallback]
    */
+  constructor(keyCallback) {
+    this.keyCallback = keyCallback;
+    this.items = {};
+  }
