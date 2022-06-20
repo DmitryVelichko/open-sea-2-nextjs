18922f5ae7240395270dc2b0a38eedaf4d597b63
Dmitry Velichko <soleil-2005@mail.ru>
2022-06-20 23:14:05 +0300

    new commit

--- algo.js
+++ algo.js
@@ -8,3 +8,15 @@ export default class DisjointSet {
     this.keyCallback = keyCallback;
     this.items = {};
   }
+
+  /**
+   * @param {*} itemValue
+   * @return {DisjointSet}
+   */
+  makeSet(itemValue) {
+    const disjointSetItem = new DisjointSetItem(itemValue, this.keyCallback);
+
+    if (!this.items[disjointSetItem.getKey()]) {
+      // Add new item only in case if it not presented yet.
+      this.items[disjointSetItem.getKey()] = disjointSetItem;
+    }
