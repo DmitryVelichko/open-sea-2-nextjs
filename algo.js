b6fc335c3d95cd6ed632f027fb061ddfff685e35
Dmitry Velichko <soleil-2005@mail.ru>
2022-06-20 23:14:12 +0300

    new commit

--- algo.js
+++ algo.js
@@ -20,3 +20,15 @@ export default class DisjointSet {
       // Add new item only in case if it not presented yet.
       this.items[disjointSetItem.getKey()] = disjointSetItem;
     }
+
+    return this;
+  }
+
+  /**
+   * Find set representation node.
+   *
+   * @param {*} itemValue
+   * @return {(string|null)}
+   */
+  find(itemValue) {
+    const templateDisjointItem = new DisjointSetItem(itemValue, this.keyCallback);
