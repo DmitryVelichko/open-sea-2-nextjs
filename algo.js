ddf19fce6a61f86fd8709b02225d911073502f3f
Dmitry Velichko <soleil-2005@mail.ru>
2022-06-23 21:12:05 +0300

    new commit

--- algo.js
+++ algo.js
@@ -20,3 +20,14 @@ export default class ComplexNumber {
   add(addend) {
     // Make sure we're dealing with complex number.
     const complexAddend = this.toComplexNumber(addend);
+
+    return new ComplexNumber({
+      re: this.re + complexAddend.re,
+      im: this.im + complexAddend.im,
+    });
+  }
+
+  /**
+   * @param {ComplexNumber|number} subtrahend
+   * @return {ComplexNumber}
+   */
