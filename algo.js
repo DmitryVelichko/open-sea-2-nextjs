206f6016301fe88a4b2a16dc299f278853e3a914
Dmitry Velichko <soleil-2005@mail.ru>
2022-06-24 23:49:16 +0300

    new commit

--- algo.js
+++ algo.js
@@ -2,3 +2,9 @@ export default class BloomFilter {
   /**
    * @param {number} size - the size of the storage.
    */
+  constructor(size = 100) {
+    // Bloom filter size directly affects the likelihood of false positives.
+    // The bigger the size the lower the likelihood of false positives.
+    this.size = size;
+    this.storage = this.createStore(size);
+  }
